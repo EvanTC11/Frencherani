52dd10020528c0e3deb811771556fa378e8e98cb
EvanTc11 <happyboyev@gmail.com>
2023-12-05 18:55:34 +0000

    Made program multithreaded

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 import time
+import threading
 import words as w
 
-words = w.words
+class Translator:
+    def __init__(self):
+        self.words = w.words
+        self.text = ""
+        self.userInput = ""
+        self.translatedWord = ""
+        self.checkWordThread = threading.Thread(target=self.__CheckWord)
+        self.loadingThread = threading.Thread(target=self.__FrencheraniLoop)
 
-text = ""
+    def __GetAns(self, usrInput : str):
+        for i in range(len(self.words)):
+            currentWords = self.words[i].getEnglish()
+            for x in range(len(currentWords)):
+                if usrInput == currentWords[x]:
+                    return self.words[i].getFrench()
+                
+    def __FrencheraniLoop(self):
+        while self.checkWordThread.is_alive():
+            print(f"frencherani est working{self.text}")
+            self.text += "."
+            time.sleep(0.5)
 
-userInput = input("Pick a word to get (in English): ")
-userInput.lower()
+    def __CheckWord(self):
+        if w.CheckForWord(self.userInput):
+            self.translatedWord = self.__GetAns(self.userInput)
+        else:
+            print("invalid input")
+            
+        time.sleep(1)
 
-def getAns(usrInput : str):
-    for i in range(len(words)):
-        if usrInput == words[i].getEnglish():
-            return words[i].getFrench()
+    def Run(self):
+        self.userInput = input("Pick a word to get (in English): ")
+        self.userInput.lower()
 
-for i in range(5):
-    print(f"frencherani est working{text}")
-    text += "."
-    time.sleep(1)
+        self.checkWordThread.start()
+        self.loadingThread.start()
+        self.checkWordThread.join()
+        print(self.translatedWord)
 
-if w.CheckForWord(userInput):
-    print(getAns(userInput))
-
-else:
-    print("invalid input")
+if __name__ == "__main__":
+    main = Translator()
+    main.Run()
